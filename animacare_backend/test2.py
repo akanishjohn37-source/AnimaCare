58bebbf9fa358f104a09e8f2da6fd45f79e834c2
A K ANISH JOHN <akanishjohn37@gmail.com>
2026-06-04 12:49:16 +0530

    chore: untrack local scratch python files

--- animacare_backend/test2.py
+++
@@ -1,8 +0,0 @@
-import sys, traceback
-from apps.citizens.models import Pet
-try:
-    Pet.objects.get(id=1).delete()
-    print("DELETE SUCCESSFUL")
-except Exception as e:
-    print("DELETE ERROR:", repr(e))
-    traceback.print_exc(file=sys.stdout)
